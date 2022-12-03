0ee447f4ca75634c0d9c277a6f361e27f3a6df22
Nicula Dragos <110388376+DragosNicula@users.noreply.github.com>
2022-12-03 13:22:26 +0200

    Add files via upload

--- src/components/User/logout.jsx
+++ src/components/User/logout.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { getAuth, signOut } from 'firebase/auth';
 import { app } from '../../firebase.js';
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 
 export function LogOut() {
     const auth = getAuth(app);
+    const navigate = useNavigate();
 
     function logOut() {
         signOut(auth).then(() => {
+            navigate('/home')
             alert("Sign Out Succesfull")
-            window.location.reload();
+            window.location.reload()
         }).catch((error) => {
             alert(error.message)
         });
